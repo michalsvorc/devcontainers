b2ef97adc9ea6f489e404aac09e3d9ede3cd6c49
Michal Svorc <dev@michalsvorc.com>
2024-05-31 18:55:27 +0200

    chore(scripts): disable debugging in aws cli

--- scripts/install_aws_cli.sh
+++ scripts/install_aws_cli.sh
@@ -17,7 +17,7 @@ set -o errexit  # Exit if any command exits with a nonzero (error) status.
 set -o nounset  # Disallow expansion of unset variables.
 set -o pipefail # Use last non-zero exit code in a pipeline.
 set -o errtrace # Ensure the error trap handler is properly inherited.
-set -o xtrace   # Enable shell script debugging mode.
+# set -o xtrace   # Enable shell script debugging mode.
 
 #===============================================================================
 # Variables
