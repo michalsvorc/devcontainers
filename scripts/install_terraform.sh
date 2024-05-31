9612ddba5e7558e6d396541b66d3ec3284e48751
Michal Svorc <dev@michalsvorc.com>
2024-05-31 19:32:43 +0200

    fix(scripts): terraform install, add tab completion

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -16,22 +16,34 @@ set -o errexit  # Exit if any command exits with a nonzero (error) status.
 set -o nounset  # Disallow expansion of unset variables.
 set -o pipefail # Use last non-zero exit code in a pipeline.
 set -o errtrace # Ensure the error trap handler is properly inherited.
-set -o xtrace   # Enable shell script debugging mode.
+# set -o xtrace   # Enable shell script debugging mode.
 
 #===============================================================================
 # Functions
 #===============================================================================
 
 main() {
-  printf 'Installing Terraform\n'
-  install "${@}"
+	printf 'Installing Terraform\n'
+	install "${@}" &&
+		generate_tab_completion
 }
 
 install() {
-  wget -O- https://apt.releases.hashicorp.com/gpg |
-    sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpgecho "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" |
-    sudo tee /etc/apt/sources.list.d/hashicorp.listsudo apt update &&
-    sudo apt install terraform
+	curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+	apt update && apt install terraform
+}
+
+generate_tab_completion() {
+	local -r config_file="${HOME}/.profile"
+	cat <<EOF >>"${config_file}"
+# Terraform tab completion
+# https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli#enable-tab-completion
+autoload -U +X compinit bashcompinit
+compinit
+bashcompinit
+complete -o nospace -C /usr/bin/terraform terraform
+EOF
 }
 
 #===============================================================================
